6cce9e08301e34ae3c03a0d7ee0da2587e609378
zhaohq <610006480@qq.com>
2018-11-18 03:08:30 +0800

    update

--- src/main/java/io/springrain/controller/IndexController.java
+++ src/main/java/io/springrain/controller/IndexController.java
@@ -1,9 +1,6 @@
 package io.springrain.controller;
 
-import io.springrain.mapper.User;
-import io.springrain.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,22 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class IndexController {
 
-    @Autowired
-    private UserMapper userMapper;
-
     @GetMapping
     public String index() {
-	    User user = userMapper.selectByPK(4);
-	    log.info("before:{}",user.toString());
-
-	    user.setName("update_" + user.getName());
-
-	    // updateByPK是从CrudMapper中继承而来的，UserMapper.xml中并没有声明该sql
-	    userMapper.updateByPK(user);
-
-	    user = userMapper.selectByPK(user.getId());
-	    log.info("after:{}", user.toString());
-
         return "index";
     }
 
